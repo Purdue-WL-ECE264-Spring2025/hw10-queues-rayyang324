a06f2047ccfcff0d2035a74a7acc05ad05150ba8
rayyang324 <yang.hsinhua.ray@gmail.com>
2025-04-04 08:21:28 -0400

    Done except number of moves

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,110 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) 
+{
+  struct list_node* newNodePtr = malloc(sizeof(struct list_node));
+  if (newNodePtr == NULL)
+  {
+    return NULL;
+  }
+  newNodePtr -> value = value;
+  newNodePtr -> next = NULL;
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+  return newNodePtr; 
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) 
+{
+  struct list_node* newVal = new_node(value);
+  newVal -> next = list -> head;
+  list -> head = newVal;
+  return;
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+void insert_at_tail(struct linked_list *list, size_t value) 
+{
+  struct list_node* newVal = new_node(value);
+  
+  
+  if (list -> head == NULL) // handles the problem where it is an empty list
+  {
+    list -> head = newVal;
+    return;
+  }
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+  struct list_node* addressToCheck = list -> head;
+  while(addressToCheck -> next!= NULL)
+  {
+    addressToCheck = addressToCheck -> next;
+  }
 
-void free_list(struct linked_list list) {}
+  addressToCheck -> next = newVal; // adding in the new value at the end
+  return;
+}
+
+size_t remove_from_head(struct linked_list *list)  
+{
+  if (list -> head == NULL) // handles empty list
+  {
+    return 0;
+  }
+  size_t headVal = (list -> head) -> value; // head value
+  struct list_node* temp = list -> head; // temp variable for list head
+  list -> head = (list -> head) -> next; // change new head
+  
+  free(temp);
+  return headVal; 
+}
+
+size_t remove_from_tail(struct linked_list *list) 
+{
+  size_t val;
+  struct list_node* addressToCheck;
+  if (list -> head == NULL)
+  {
+    return 0;
+  }
+  if ((list -> head) -> next == NULL)
+  {
+    val = (list -> head) -> value;
+    free(list -> head);
+    list -> head = NULL;
+    return val;
+  }
+
+  addressToCheck = list -> head;
+  while((addressToCheck -> next) -> next != NULL)
+  {
+    addressToCheck = addressToCheck -> next;
+  }
+  val = (addressToCheck -> next) -> value;
+  free(addressToCheck -> next);
+  addressToCheck -> next = NULL;
+
+  return val; 
+}
+
+
+void free_list(struct linked_list* list) 
+{
+  if (list -> head == NULL) // if empty list
+  {
+    return;
+  }
+
+  struct list_node* temp = list -> head;
+  struct list_node* temp2 = NULL;
+
+  do
+  {
+    temp2 = temp -> next;
+    free(temp);
+    temp = temp2;
+  }while(temp -> next != NULL);
+  
+  return;
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,20 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) 
+{
+  size_t serializedState = serialize(state);
+  insert_at_tail(&(q -> data), serializedState);
+  return;
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) 
+{ 
+  struct game_state removedGameState = deserialize((remove_from_head(&q -> data)));
+  return removedGameState; 
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+int number_of_moves(struct game_state start) 
+{
+  return 0; 
+}
